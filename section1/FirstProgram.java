e18d60b1567966af0c053cd0ea58b3065b91c84b
Erik Black <blac.erik@gmail.com>
2024-04-07 09:45:23 -0600

    adding files

---
+++ section1/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println( "Printing");
+        System.out.println( "Java > Python");
+        System.out.println( "More printing");
+        System.out.println( "even more printing");
+    }
+}
